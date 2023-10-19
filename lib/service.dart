561d8a14f40269eb9728945df8fd290c31e781aa
César Pedraza <cpedraza@unal.edu.co>
2023-10-19 08:33:09 -0500

    apply formatting

--- lib/service.dart
+++ lib/service.dart
@@ -1,27 +1,25 @@
 import 'package:sqflite/sqflite.dart';
 
 class Service {
-    final Database database;
+  final Database database;
 
-    Service(this.database);
+  Service(this.database);
 
-    static Future<Service> withDB() async => Service(await openOrCreateDB());
+  static Future<Service> withDB() async => Service(await openOrCreateDB());
 
-    static Future<Database> openOrCreateDB() async => openDatabase(
-        'freelog.db',
-        onCreate: (db, version) => db.execute(
-            '''
-                CREATE TABLE TRAINING_SESSION (
-                    id INTEGER PRIMARY KEY, 
-                    date TEXT
-                )
+  static Future<Database> openOrCreateDB() async => openDatabase(
+    'freelog.db',
+    onCreate: (db, version) => db.execute('''
+      CREATE TABLE TRAINING_SESSION (
+        id INTEGER PRIMARY KEY, 
+        date TEXT
+      )
 
-                CREATE TABLE EXERCISE_EXECUTION (
-                    id INTEGER PRIMARY KEY,
-                    training_session INTEGER,
-                    FOREIGN KEY (training_session) REFERENCES TRAINING_SESSION(id) 
-                )
-            '''
-        ),
-    );
+      CREATE TABLE EXERCISE_EXECUTION (
+        id INTEGER PRIMARY KEY,
+        training_session INTEGER,
+        FOREIGN KEY (training_session) REFERENCES TRAINING_SESSION(id) 
+      )
+    '''),
+  );
 }
