4e1b3ac711bc54556e67a5f66f0300f5bf340e26
César Pedraza <cpedraza@unal.edu.co>
2023-10-19 09:39:56 -0500

    first db test

--- lib/service.dart
+++ lib/service.dart
@@ -9,12 +9,12 @@ class Service {
 
   static Future<Database> openOrCreateDB() async => openDatabase(
     'freelog.db',
+    version: 1,
     onCreate: (db, version) => db.execute('''
       CREATE TABLE TRAINING_SESSION (
-        id INTEGER PRIMARY KEY, 
+        id INTEGER PRIMARY KEY,
         date TEXT
       )
-
       CREATE TABLE EXERCISE_EXECUTION (
         id INTEGER PRIMARY KEY,
         training_session INTEGER,
---
+++ test/service_test.dart
@@ -0,0 +1,24 @@
+import 'package:freelog/service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+Future main() async {
+
+  setUpAll(() {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  });
+
+  tearDownAll(() {
+    // Maybe delete the database here
+  });
+
+  group('Service()', () {
+    group('.openOrCreateDB()', () {
+      test('should create the DB if it is not there', () async {
+        final Database db = await Service.openOrCreateDB();
+        expect(await databaseExists('freelog.db'), isTrue);
+      });
+    });
+  });
+}
