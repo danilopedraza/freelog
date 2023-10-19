3e621e90702f4ffc805709fbac827f4d3fb0ad09
César Pedraza <cpedraza@unal.edu.co>
2023-10-18 21:45:50 -0500

    first db scheme

--- lib/service.dart
+++ lib/service.dart
@@ -10,7 +10,18 @@ class Service {
     static Future<Database> openOrCreateDB() async => openDatabase(
         'freelog.db',
         onCreate: (db, version) => db.execute(
-            'CREATE TABLE TRAINING_SESSIONS (id INTEGER PRIMARY KEY, date TEXT)'
+            '''
+                CREATE TABLE TRAINING_SESSION (
+                    id INTEGER PRIMARY KEY, 
+                    date TEXT
+                )
+
+                CREATE TABLE EXERCISE_EXECUTION (
+                    id INTEGER PRIMARY KEY,
+                    training_session INTEGER,
+                    FOREIGN KEY (training_session) REFERENCES TRAINING_SESSION(id) 
+                )
+            '''
         ),
     );
 }
