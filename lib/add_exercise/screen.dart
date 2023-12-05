09ca2eb006424d43f23eab61bf7ead157a6ab8b3
César Pedraza <cpedraza@unal.edu.co>
2023-12-05 16:11:00 -0500

    add new exercise screen

---
+++ lib/add_exercise/screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AddExercise extends StatelessWidget {
+  const AddExercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Añadir ejercicio',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            'Nombre',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Escribe el nombre aquí',
+            ),
+          ),
+          const Text(
+            'Categoría',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const DropdownMenu(
+            dropdownMenuEntries: [],
+            hintText: 'Selecciona una categoría',
+          ),
+          const Text(
+            'Variable principal',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const DropdownMenu(
+            dropdownMenuEntries: [],
+            hintText: 'Selecciona una variable',
+          ),
+          const Text(
+            'Variable secundaria',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const DropdownMenu(
+            dropdownMenuEntries: [],
+            hintText: 'Selecciona una variable',
+          ),
+        ]
+            .map((w) => Padding(padding: const EdgeInsets.all(8.0), child: w))
+            .toList(),
+      ),
+    );
+  }
+}
